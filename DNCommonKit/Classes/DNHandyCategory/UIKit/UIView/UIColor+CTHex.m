8dfc551068f6e33611c59a4c3e95c40a8fb26902
陈金铭 <418589912@qq.com>
2020-04-10 13:53:35 +0800

    代码格式整理

--- DNCommonKit/Classes/DNHandyCategory/UIKit/UIView/UIColor+CTHex.m
+++ DNCommonKit/Classes/DNHandyCategory/UIKit/UIView/UIColor+CTHex.m
@@ -66,10 +66,10 @@
 
 // 随机颜色
 + (UIColor *)ct_randomColor {
-    NSInteger aRedValue =arc4random() %255;
-    NSInteger aGreenValue =arc4random() %255;
-    NSInteger aBlueValue =arc4random() %255;
-    UIColor *randColor = [UIColor colorWithRed:aRedValue /255.0f green:aGreenValue /255.0f blue:aBlueValue /255.0f alpha:1.0f];
+    NSInteger aRedValue = arc4random() % 255;
+    NSInteger aGreenValue =arc4random() % 255;
+    NSInteger aBlueValue =arc4random() % 255;
+    UIColor *randColor = [UIColor colorWithRed:aRedValue / 255.0f green:aGreenValue / 255.0f blue:aBlueValue / 255.0f alpha:1.0f];
     return randColor;
 }
 
